bd769bf78ac2acabe81ae537e6c792be117ad7e6
Rafael Honório <rafaelxhs@gmail.com>
2023-03-24 23:08:54 -0300

    Teste 2

--- src/pages/teste.tsx
+++ src/pages/teste.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,10 +22,8 @@ export default function Home() {
             <code className={styles.code}>src/pages/teste.tsx</code>
           </p>
           <div>
-            <a
+            <Link
               href="/"
-              target="_blank"
-              rel="noopener noreferrer"
             >
               By{' '}
               <Image
@@ -35,7 +34,7 @@ export default function Home() {
                 height={24}
                 priority
               />
-            </a>
+            </Link>
           </div>
         </div>
 
